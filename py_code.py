daefbb84f0ba1badcd1aa266ff5e30d4ad70619a
MihirNagarkar <mihirnagarkar9@gmail.com>
2025-02-15 12:35:41 +0530

    First

---
+++ py_code.py
@@ -0,0 +1,9 @@
+
+def check_even_odd(num):
+    if num % 2 == 0:
+        print(f"{num} is Even.")
+    else:
+        print(f"{num} is Odd.")
+
+num = int(input("Enter a number: "))
+check_even_odd(num)
